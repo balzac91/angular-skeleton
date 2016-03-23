052fdc9e305c88d09576af1f7dadb2a47f91b3b8
Kamil Fus <balzac91@gmail.com>
2016-03-23 22:08:41 +0100

    gulpfile and eslint

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,12 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+  eslint = require('gulp-eslint');
 
-gulp.task('default', function() {
+gulp.task('default', function () {
   console.log('gulp');
+});
+
+gulp.task('lint', function() {
+  gulp.src('./app/**/*.js')
+    .pipe(eslint())
+    .pipe(eslint.format());
 });
