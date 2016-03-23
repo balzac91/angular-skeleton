ba71c8cc76ded0066cb5adbeb7019c93ac16607a
Kamil Fus <balzac91@gmail.com>
2016-03-23 22:35:38 +0100

    gulpfile.js refactor

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,12 @@
 var gulp = require('gulp'),
   eslint = require('gulp-eslint');
 
-gulp.task('default', function () {
-  console.log('gulp');
-});
-
-gulp.task('lint', function() {
+gulp.task('lint', function () {
   gulp.src('./app/**/*.js')
     .pipe(eslint())
     .pipe(eslint.format());
+});
+
+gulp.task('watch', ['lint'], function () {
+  gulp.watch(['./app/**/*.js'], ['lint']);
 });
