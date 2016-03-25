288a64980a002e72cf64aeb073238c98d7955913
Kamil Fus <balzac91@gmail.com>
2016-03-25 21:01:39 +0100

    gulp build task

--- gulpfile.js
+++ gulpfile.js
@@ -4,10 +4,11 @@ var gulp = require('gulp'),
   uglify = require('gulp-uglify'),
   clean = require('gulp-clean'),
   templateCache = require('gulp-angular-templatecache'),
-  es = require('event-stream');
+  inject = require('gulp-inject'),
+  eventStream = require('event-stream');
 
 gulp.task('lint', function () {
-  gulp.src('./app/**/*.js')
+  return gulp.src('./app/**/*.js')
     .pipe(eslint())
     .pipe(eslint.format());
 });
@@ -17,26 +18,34 @@ gulp.task('watch', ['lint'], function () {
 });
 
 gulp.task('clean', function () {
-  gulp.src('./dist/*')
+  return gulp.src('./dist/*')
     .pipe(clean());
 });
 
 gulp.task('jsmin', function () {
-  var src = gulp.src(['./app/bower_components/angular/angular.min.js', './app/bower_components/angular-ui-router/release/angular-ui-router.min.js']);
-  var src2 = gulp.src(['./app/**/*.js', '!./app/bower_components/**/*.js']);
-  var src3 = gulp.src(['./app/**/*.html', '!./app/index.html', '!./bower_components/**/*.html'])
-    .pipe(templateCache('templates.js', {
-      module: 'app',
-      root: './'
-    }));
+  var bowerComponents = gulp.src(['./app/bower_components/angular/angular.min.js', './app/bower_components/angular-ui-router/release/angular-ui-router.min.js']),
+    sources = gulp.src(['./app/**/*.js', '!./app/bower_components/**/*.js']),
+    templates = gulp.src(['./app/**/*.html', '!./app/index.html', '!./bower_components/**/*.html'])
+      .pipe(templateCache('templates.js', {
+        module: 'app',
+        root: './'
+      }));
 
-  es.merge(src, src2, src3)
+  return eventStream.merge(bowerComponents, sources, templates)
     .pipe(concat('app.min.js'))
     .pipe(uglify())
     .pipe(gulp.dest('./dist/'));
 });
 
 gulp.task('build', ['clean', 'jsmin'], function () {
-  gulp.src('./app/index.html')
+  var appSrc = gulp.src('./dist/app.min.js', {
+    read: false
+  });
+
+  return gulp.src('./app/index.html')
+    .pipe(inject(appSrc, {
+      ignorePath: 'dist',
+      addRootSlash: false
+    }))
     .pipe(gulp.dest('./dist/'));
 });
