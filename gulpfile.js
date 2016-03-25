27d4dff61b214c52ce5e939aa748d2b5917e1ed1
Kamil Fus <balzac91@gmail.com>
2016-03-25 20:25:12 +0100

    add jsmin and init build tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,10 @@
 var gulp = require('gulp'),
   eslint = require('gulp-eslint'),
   concat = require('gulp-concat'),
-  uglify = require('gulp-uglify');
+  uglify = require('gulp-uglify'),
+  clean = require('gulp-clean'),
+  templateCache = require('gulp-angular-templatecache'),
+  es = require('event-stream');
 
 gulp.task('lint', function () {
   gulp.src('./app/**/*.js')
@@ -13,9 +16,27 @@ gulp.task('watch', ['lint'], function () {
   gulp.watch(['./app/**/*.js'], ['lint']);
 });
 
-gulp.task('min', function () {
-  gulp.src('./app/**/*.js')
+gulp.task('clean', function () {
+  gulp.src('./dist/*')
+    .pipe(clean());
+});
+
+gulp.task('jsmin', function () {
+  var src = gulp.src(['./app/bower_components/angular/angular.min.js', './app/bower_components/angular-ui-router/release/angular-ui-router.min.js']);
+  var src2 = gulp.src(['./app/**/*.js', '!./app/bower_components/**/*.js']);
+  var src3 = gulp.src(['./app/**/*.html', '!./app/index.html', '!./bower_components/**/*.html'])
+    .pipe(templateCache('templates.js', {
+      module: 'app',
+      root: './'
+    }));
+
+  es.merge(src, src2, src3)
     .pipe(concat('app.min.js'))
     .pipe(uglify())
+    .pipe(gulp.dest('./dist/'));
+});
+
+gulp.task('build', ['clean', 'jsmin'], function () {
+  gulp.src('./app/index.html')
     .pipe(gulp.dest('./dist/'));
 });
