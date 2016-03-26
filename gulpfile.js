0f370fdd8fef301127fad6602d33432acb4a02b7
Kamil Fus <balzac91@gmail.com>
2016-03-26 15:34:05 +0100

    fix clean issue

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,13 @@ var gulp = require('gulp'),
   eslint = require('gulp-eslint'),
   concat = require('gulp-concat'),
   uglify = require('gulp-uglify'),
-  clean = require('gulp-clean'),
   less = require('gulp-less'),
   cssmin = require('gulp-cssmin'),
   templateCache = require('gulp-angular-templatecache'),
   inject = require('gulp-inject'),
   eventStream = require('event-stream'),
-  rename = require('gulp-rename');
+  rename = require('gulp-rename'),
+  del = require('del');
 
 gulp.task('lint', function () {
   return gulp.src('./app/**/*.js')
@@ -21,20 +21,29 @@ gulp.task('watch', ['lint'], function () {
 });
 
 gulp.task('clean', function () {
-  return gulp.src('./dist/*')
-    .pipe(clean());
+  return del('./dist');
+});
+
+gulp.task('fonts', function () {
+  return gulp.src('./app/bower_components/bootstrap/fonts/*.*')
+    .pipe(gulp.dest('./dist/fonts/'));
 });
 
 gulp.task('cssmin', function () {
   return gulp.src('./app/css/app.less')
-    .pipe(less())
+    .pipe(less({
+      'relative-urls': true
+    }))
     .pipe(cssmin())
     .pipe(rename({suffix: '.min'}))
     .pipe(gulp.dest('./dist/'));
 });
 
 gulp.task('jsmin', function () {
-  var bowerComponents = gulp.src(['./app/bower_components/angular/angular.min.js', './app/bower_components/angular-ui-router/release/angular-ui-router.min.js']),
+  var bowerComponents = gulp.src(['./app/bower_components/angular/angular.min.js',
+    './app/bower_components/angular-ui-router/release/angular-ui-router.min.js',
+      './app/bower_components/jquery/dist/jquery.min.js',
+    './app/bower_components/bootstrap/dist/js/bootstrap.min.js']),
     sources = gulp.src(['./app/**/*.js', '!./app/bower_components/**/*.js']),
     templates = gulp.src(['./app/**/*.html', '!./app/index.html', '!./bower_components/**/*.html'])
       .pipe(templateCache('templates.js', {
@@ -48,7 +57,7 @@ gulp.task('jsmin', function () {
     .pipe(gulp.dest('./dist/'));
 });
 
-gulp.task('build', ['clean', 'jsmin', 'cssmin'], function () {
+gulp.task('create-app', ['jsmin', 'cssmin', 'fonts'], function () {
   var jsSrc = gulp.src('./dist/app.min.js', {
     read: false
   });
@@ -67,4 +76,8 @@ gulp.task('build', ['clean', 'jsmin', 'cssmin'], function () {
       addRootSlash: false
     }))
     .pipe(gulp.dest('./dist/'));
+});
+
+gulp.task('build', ['clean'], function () {
+  return gulp.start('create-app');
 });
