774f6ed6930689dd9071f9f1cd82c8678ad2a4ab
Kamil Fus <balzac91@gmail.com>
2016-03-29 20:10:18 +0200

    add comments to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -12,20 +12,53 @@ var gulp = require('gulp'),
   del = require('del'),
   paths = require('./gulp.config.json');
 
+/*******************************************[MAIN TASKS]******************************************/
+
+/**
+ * Build optimized app
+ */
+gulp.task('build', ['clean'], function () {
+  return gulp.start('create-app');
+});
+
+/**
+ * Serve dev environment with JS and less watcher
+ */
+gulp.task('serve', ['watch'], function() {
+  gulp.src(paths.app)
+    .pipe(webserver({
+      open: true
+    }));
+});
+
+/******************************************[HELPER TASKS]*****************************************/
+
+/**
+ * Remove dist directory
+ */
 gulp.task('clean', function () {
   return del(paths.dist);
 });
 
-gulp.task('fonts', function () {
+/**
+ * Copy fonts to dist directory
+ */
+gulp.task('copy-fonts', function () {
   return gulp.src(paths.fonts)
     .pipe(gulp.dest(paths.dist + 'fonts/'));
 });
 
-gulp.task('images', function () {
+/**
+ * Copy images to dist directory
+ */
+gulp.task('copy-images', function () {
   return gulp.src(paths.images)
     .pipe(gulp.dest(paths.dist + 'images/'));
 });
 
+/**
+ * Minify and bundle less files
+ */
 gulp.task('cssmin', function () {
   return gulp.src(paths.appLess)
     .pipe(less({
@@ -36,6 +69,9 @@ gulp.task('cssmin', function () {
     .pipe(gulp.dest(paths.dist));
 });
 
+/**
+ * Minify and bundle JS files
+ */
 gulp.task('jsmin', function () {
   var bowerComponents = gulp.src(paths.vendorJs),
     sources = gulp.src(paths.js),
@@ -51,7 +87,10 @@ gulp.task('jsmin', function () {
     .pipe(gulp.dest(paths.dist));
 });
 
-gulp.task('create-app', ['jsmin', 'cssmin', 'fonts', 'images'], function () {
+/**
+ * Create optimized app
+ */
+gulp.task('create-app', ['jsmin', 'cssmin', 'copy-fonts', 'copy-images'], function () {
   var jsSrc = gulp.src(paths.dist + paths.jsMinified, {
     read: false
   });
@@ -72,10 +111,18 @@ gulp.task('create-app', ['jsmin', 'cssmin', 'fonts', 'images'], function () {
     .pipe(gulp.dest(paths.dist));
 });
 
-gulp.task('build', ['clean'], function () {
-  return gulp.start('create-app');
+/**
+ * Lint JS files using ESlint
+ */
+gulp.task('lint', function () {
+  return gulp.src(paths.js)
+    .pipe(eslint())
+    .pipe(eslint.format());
 });
 
+/**
+ * Compile less into css for dev server
+ */
 gulp.task('css', function () {
   return gulp.src(paths.less)
     .pipe(less({
@@ -84,20 +131,10 @@ gulp.task('css', function () {
     .pipe(gulp.dest(paths.app));
 });
 
-gulp.task('lint', function () {
-  return gulp.src(paths.js)
-    .pipe(eslint())
-    .pipe(eslint.format());
-});
-
+/**
+ * Watch JS and less file
+ */
 gulp.task('watch', ['lint'], function () {
   gulp.watch(paths.js, ['lint']);
   gulp.watch(paths.less, ['css']);
-});
-
-gulp.task('serve', ['watch'], function() {
-  gulp.src(paths.app)
-    .pipe(webserver({
-      open: true
-    }));
 });
